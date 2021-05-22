508b5da1715ef4c4e7fc3e46cfd3de753f26107b
Steffen <12001881@student.pxl.be>
2021-05-22 11:58:00 +0200

    argumenten uitlezen en juist gebruiken
    de memcpy nog nakijken

--- main.c
+++ main.c
@@ -1,61 +1,58 @@
 #include "personalLib.h"
 #include <stdio.h>
+#include <string .h>
+//voorbeelden Project.exe -c -s inputfile.txt -i inputbmpfile.bmp -o outputbmpfile.bmp
+			//Project.exe -d -i memeOut.bmp -o bericht.txt
 
-
-
-int main() 
-{
 int main(int argc, char* argv[]) {
-	if (argc == 2 && strcmp(argv[1], "--help") == 0) //--help
+	if (argc > 1)
 	{
-		helpMenu();
+		if(argc == 1 && strcmp(argv[2], "--help")==0)
+		{
+			helpMenu();
+		}
+		else if(argc == 8 && strcmp(argv[2], "-c")==0)
+		{
+			for(int i = 0; i < argc; i++)
+			{
+				if (strcmp(argv[i], "-s") == 0)
+				{
+					memcpy(InputTXT, argv[i+1], 20);
+				}
+				if (strcmp(argv[i], "-i") == 0)
+				{
+					memcpy(BMPINPUTFILE, argv[i+1], 20);
+				}
+				if (strcmp(argv[i], "-o") == 0)
+				{
+					memcpy(BMPOUTPUTFILE, argv[i+1], 20);
+				}
+			}
+			i=0;
+			code();
+		}
+		else if(argc == 6 && strcmp(argv[2], "-d")==0)
+		{
+			for(int i = 0; i < argc; i++)
+			{
+				if (strcmp(argv[2], "-i") == 0)
+				{
+					memcpy(BMPINPUTFILE, argv[3], 20);
+				}
+				if (strcmp(argv[4], "-o") == 0)
+				{
+					memcpy(OutputTXT, argv[5], 20);
+				}	
+			}
+			i=0;
+			decode();
+		}
+		else
+		{
+			printf("Fout!!! \nFout argument kijk in de --help");
+			exit(EXIT_FAILURE);
+		}
 	}
-	else if (argc == 8 && strcmp(argv[1], "-c") == 0)
-	{
-		//Project.exe -c -s inputfile.txt -i meme.bmp -o memeOut.bmp
-		if (argc == 8 && strcmp(argv[2], "-s") == 0)
-		{
-			//InputTXT geen vaste grootte geven //werkt niet
-			//InputTXT = (char*)malloc(strlen(argv[3]));
-			//memcpy(InputTXT, argv[3], strlen(argv[3]));
-
-			memcpy(InputTXT, argv[3], 20);
-		}
-		if (argc == 8 && strcmp(argv[4], "-i") == 0)
-		{
-			memcpy(BMPINPUTFILE, argv[5], 20);
-		}
-		if (argc == 8 && strcmp(argv[6], "-o") == 0)
-		{
-			memcpy(BMPOUTPUTFILE, argv[7], 20);
-		}
-		compressDecompress('c');
-	}
-	else if (argc == 6 && strcmp(argv[1], "-d") == 0)
-	{
-		//Project.exe -d -i memeOut.bmp -o bericht.txt
-
-		if (argc == 6 && strcmp(argv[2], "-i") == 0)
-		{
-			memcpy(BMPINPUTFILE, argv[3], 20);
-		}
-		if (argc == 6 && strcmp(argv[4], "-o") == 0)
-		{
-			memcpy(OutputTXT, argv[5], 20);
-		}
-		compressDecompress('d');
-	}
-	else
-	{
-		printf("Fout!!! \nNiet de juiste argumenten \nKijk in --help voor de uitleg \n");
-		exit(EXIT_FAILURE);
-	}
-	
-	code();
-	decode();
-	
-	helpMenu();
-	
 	return 0;
 }
 
